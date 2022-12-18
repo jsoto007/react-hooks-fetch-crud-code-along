25793a3c8f6920a6006199b163bd94a1059bc732
Jose Soto <jsoto7087@gmail.com>
2022-12-18 17:11:00 -0500

    completed DELETE, POST, PATCH

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-function Item({ item, onUpdateItem }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE", 
+    })
+      .then((r) => r.json())
+      .then(() => onDeleteItem(item))
+  }
 
   function handleAddToCartClick() {
 
@@ -24,7 +32,7 @@ function Item({ item, onUpdateItem }) {
       <button className={item.isInCart ? "remove" : "add"} onClick={handleAddToCartClick}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
